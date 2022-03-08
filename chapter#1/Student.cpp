31fb0bb6c3baf4c52a9a88dc1a19297a6dc19345
tothedoor <tothedoor98@gmail.com>
2022-03-08 14:49:13 +0900

    chapter 1 is changed

---
+++ chapter#1/Student.cpp
@@ -0,0 +1,3 @@
+#include "Student.h"
+
+Student& Student::operator = ()
--- chapter#1/student.h
+++ chapter#1/student.h
@@ -2,20 +2,39 @@
 #ifndef STUDENT
 #define STUDENT
 
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
 class Student
 {
 public:
 	// data members
 	int Identifier; char Name[11]; char Address[16];
-	date DateOfFirstEnrollment[16]; int NumberOfCreditHourse;
+	char DateOfFirstEnrollment[8]; int NumberOfCreditHourse;
 	// method
 	Student(); // default constructor
+	Student& operator = (const Student&); // assign operator
+
+	// 필드값 갱신 methods
+	void setIdentifier(int newIdentifier);
+	void setName(char newName[]);
+	void setAddress(char newAddr);
+	void setDateOfFirstEnrollment(char newDate[]);
+	void setNumberOfCreditHourse(int newValue);
+	
+	// 이수학점 증가 method
+	void addCreditHourse(int value);
 };
 
+/*
 Student::Student()
 {//constructor
+
 	LastName[0] = 0; FirstName[0] = 0; Address[0] = 0;
 	City[0] = 0; State[0] = 0; ZipCode[0] = 0;
 }
+*/
 
 #endif
